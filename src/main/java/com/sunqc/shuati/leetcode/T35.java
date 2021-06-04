01d6d502e881c183a9a29935641d5c2f422f52f8
sunqichao <798831181@qq.com>
2021-06-04 23:21:35 +0800

    leetcode第35题，搜索插入位置

--- src/main/java/com/sunqc/shuati/leetcode/T35.java
+++ src/main/java/com/sunqc/shuati/leetcode/T35.java
@@ -4,13 +4,13 @@ package com.sunqc.shuati.leetcode;
  * 搜索插入位置
  */
 public class T35 {
-    public int searchInsert( int nums[], int target){
-        if (target <= nums[0]){
+    public int searchInsert(int nums[], int target) {
+        if (target <= nums[0]) {
             return 0;
         }
-        for (int i = 0; i < nums.length - 1; i++){
-            if (target > nums[i] && i < nums.length && target <= nums[i+1]){
-                return i+1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (target > nums[i] && i < nums.length && target <= nums[i + 1]) {
+                return i + 1;
             }
         }
         return nums.length;
@@ -19,9 +19,8 @@ public class T35 {
     public static void main(String[] args) {
         T35 t = new T35();
         int[] nums = new int[]{
-                1,2,3,4,5
+                1, 2, 3, 4, 5
         };
         System.out.println(t.searchInsert(nums, 7));
-
     }
 }
