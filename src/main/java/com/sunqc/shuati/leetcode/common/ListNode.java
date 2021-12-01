677f79e153cd2d340945935f9b212978ca9b7e9c
798831181 <798831181@qq.com>
2021-12-02 00:15:54 +0800

    new： 新增链表节点公共类

---
+++ src/main/java/com/sunqc/shuati/leetcode/common/ListNode.java
@@ -0,0 +1,16 @@
+package com.sunqc.shuati.leetcode.common;
+
+/**
+ * @author : [sun_qichao]
+ * @version : [v1.0]
+ * @className : TestNode
+ * @description : 链表节点公共类
+ * @createTime : [2021/12/1 11:55 下午]
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
